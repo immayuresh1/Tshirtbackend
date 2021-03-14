a021a8bb3ef17df63e21bf98d0713d43eebda8b0
immayuresh1 <mayureshmusale12@gmail.com>
2021-03-14 23:26:51 +0530

    added validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var router = express.Router();
 const {signup,signout}= require("../controllers/auth")
+const {check,validationResult} = require("express-validator")
 
-
-router.post("/signup",signup)
+router.post("/signup",[
+    check("name","name should be at least 3 char").isLength({min:3}),
+    check("email","email is required").isEmail(),
+    check("password","password should be atleast 3 chat").isLength({min:3})
+],signup)
 
 router.get("/signout", signout);
 
