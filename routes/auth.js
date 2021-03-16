222e3581790d1fe9a462bb90393fce78ed9d959c
immayuresh1 <mayureshmusale12@gmail.com>
2021-03-16 23:56:11 +0530

    added signin route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const {signup,signout}= require("../controllers/auth")
+const {signup,signout,signin}= require("../controllers/auth")
 const {check,validationResult} = require("express-validator")
 
 router.post("/signup",[
@@ -8,6 +8,12 @@ router.post("/signup",[
     check("email","email is required").isEmail(),
     check("password","password should be atleast 3 chat").isLength({min:3})
 ],signup)
+
+
+router.post("/signin",[
+    check("email","email is required").isEmail(),
+    check("password","password field is required").isLength({min:3})
+],signin)
 
 router.get("/signout", signout);
 
