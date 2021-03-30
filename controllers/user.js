d25ce8521da27f78b6c1f24bfd48d039369817f3
immayuresh1 <mayureshmusale12@gmail.com>
2021-03-30 23:12:33 +0530

    f

--- controllers/user.js
+++ controllers/user.js
@@ -8,11 +8,15 @@ exports.getUserById = (req, res, next, id) => {
       });
     }
     req.profile = user;
-    next()
+    next();
   });
 };
 
-exports.getUser = (req,res)=>{
-    // get back here for password
-    return res.json (req.profile)
-}
+exports.getUser = (req, res) => {
+  // get back here for password
+  req.profile.salt = undefined;
+  req.profile.encry_password = undefined;
+  req.profile.createdAt = undefined;
+  req.profile.updatedAt = undefined;
+  return res.json(req.profile);
+};
