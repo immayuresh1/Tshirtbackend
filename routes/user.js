6489a4505d27bef6438289c665a02697f197cbfe
immayuresh1 <mayureshmusale12@gmail.com>
2021-04-05 23:22:11 +0530

    user route

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router()
 
-const {getUserById, getUser,getAllUsers} = require("../controllers/user")
+const {getUserById, getUser,getAllUsers,updateUser} = require("../controllers/user")
 const {isSignedIn,isAuthenticated,isAdmin} = require("../controllers/auth")
 
 router.param("userId",getUserById)
 
 router.get("/user/:userId",isSignedIn,isAuthenticated, getUser)
+router.put("/user/:userId",isSignedIn,isAuthenticated, getUser,updateUser)
 
-router.get("/users",getAllUsers)
+
 
 module.exports = router
